d0d29c66c58546a4b5d03b99289cdb4b5fc93b6e
Cuzeth <59286294+Cuzeth@users.noreply.github.com>
2026-04-08 01:19:24 -0700

    xcodeproj

---
+++ hushTests/hushTests.swift
@@ -0,0 +1,17 @@
+//
+//  hushTests.swift
+//  hushTests
+//
+
+import Testing
+@testable import hush
+
+struct hushTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
